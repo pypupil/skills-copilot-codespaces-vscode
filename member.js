d550553fdf9758aa3ff7ec09c2832aa39d04b740
pypupil <71753110+pypupil@users.noreply.github.com>
2023-07-25 07:42:56 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember()
+{
+    var member = document.getElementById("member").value;
+    var member = parseInt(member);
+    var member = member + 1;
+    document.getElementById("member").value = member;
+}
+
